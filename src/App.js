5e22e5ef364d28e83717cf553c65e4749ef02643
ayu-ayushisood <sood.ayushi30@gmail.com>
2021-05-20 23:05:49 +0530

    remove temp design

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="main_app_container">
-      {/* <img src= "../assets/images/macbook.png"/> */}
       
-
-      <div className="mac_container">
-        <div className="mac_image"> 
-          <div className="terminal">
-            <div className="text_load">profile.load();</div>  
-            {/* <div className="loader" />   */}
-          </div>
-        </div>
-      </div>
-      
-      <div className="table_container">
-        <div className="table"></div>
-      </div>
     </div>
   );
 }
