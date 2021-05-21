1c02fbbd140ddc6351be8f2e8f1340bd1f107e37
ayu-ayushisood <sood.ayushi30@gmail.com>
2021-05-21 09:26:22 +0530

    add left side content for home and convert functions to ES6

--- src/Screens/home/index.js
+++ src/Screens/home/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './style.css';
 
-function About() {
+const Home = () => {
     return (
-        <div >
-            In Home
+        <div className="home">
+            <div className="left-section">
+                <h1>Hello!</h1>
+                <h2>I'm Ayushi Sood</h2>
+                <h3>Frontend Developer - Web App and Mobile App Developer</h3>
+            </div>
+            <div className="right-section">
+
+            </div>
         </div>
     );
 }
 
-export default About;
+export default Home;
--- src/Screens/work/index.js
+++ src/Screens/work/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Work() {
+const Work = () => {
     return (
         <div >
             In Work
