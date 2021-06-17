b3f66f0167f18350fe58530889e61252819eec3e
ayu-ayushisood <sood.ayushi30@gmail.com>
2021-06-17 23:05:52 +0530

    add companies data according to the json

--- src/Screens/experience/index.js
+++ src/Screens/experience/index.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import './style.css';
-import Sidebar from '../../components/Sidebar'
-import Heading from '../../components/Header'
+import Sidebar from '../../components/Sidebar';
+import Heading from '../../components/Header';
+import Company from '../../components/Company';
+import experienceData from '../../common/experienceData.json';
 
 const Experience = () => {
+    
+    const getRoles = (roles) => {
+        let roleArr = roles.map( (role, ind) =>
+            <ul key={ind}>
+                <li>{role}</li>
+            </ul>
+        )
+        return roleArr;
+    }
+
+    const experiences = () => {
+        let data = experienceData.data.map(company =>
+            <Company
+                key={company.id}
+                logo= {company.logo}
+                title= {company.title}
+                subtitle= {company.heading}
+                companyName= {company.name}
+                roles= {getRoles(company.roles)}
+                milestone= {company.milestone}
+            />
+        )
+        return data;
+    }
     return (
         <div className="secondary-screens-container">
-           <Sidebar />
+            <Sidebar />
             <div className="right-section">
-                <Heading 
-                    name = 'Professional Experience'
+                <Heading
+                    name='Professional Experience'
                 />
+                {experiences()}
             </div>
         </div>
     );
---
+++ src/components/Company/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './style.css';
+
+const Company = (props) => {
+
+    const isMilestone = Object.entries(props.milestone).length > 0;
+    console.log("isMilestone?", isMilestone);
+
+    const getRoles = (roles) => {
+        let roleArr = roles.map( (role, ind) =>
+            <ul key={ind}>
+                <li>{role}</li>
+            </ul>
+        )
+        return roleArr;
+    }
+
+    return (
+        <div className="company-info">
+            <div className="company-logo">
+                <img src={props.logo} />
+            </div>
+            <div className="details-section">
+                <div className="title">{props.title}</div>
+                <div>{props.subtitle}</div>
+                <div>{props.companyName}</div>
+                <div className="roles">Roles and Responsibilities</div>
+                {props.roles}
+                {isMilestone &&
+                    <>
+                        <div className="title">{props.milestone.title}</div>
+                        <div>{props.milestone.heading}</div>
+                        <div className="roles">Roles and Responsibilities</div>
+                        {getRoles(props.milestone.roles)}
+                    </>
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Company;
